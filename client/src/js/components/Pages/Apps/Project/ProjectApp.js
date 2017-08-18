33bf7d708110520c2bf3a2036a367e7c21f2a2dd
Amila95 <amilawicramarathna95@gmail.com>
2017-08-18 10:58:09 +0530

    added project
    basic functions

--- client/src/js/components/Pages/Apps/Project/ProjectApp.js
+++ client/src/js/components/Pages/Apps/Project/ProjectApp.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { connect } from "react-redux"
 import SideMenu from "../../Components/SideMenu.js"
+import PickProjects from "./PickProjects"
+import ViewProject from "./ViewProject"
+
 
 @connect((store) => {
 	return {//props
 		url: store.url,
-		app: store.apps.Project
+		app: store.apps.Project,
 	}
 },)
 export default class ProjectApp extends React.Component {
@@ -16,70 +19,50 @@ export default class ProjectApp extends React.Component {
 		var tabViewID = this.props.url[2].viewID
 		switch(tabViewID){
 			case "ProjectAll":{
-				return(<PickCustomer />)
-				break	
-			}
-			case "CustomerNewCustomer":{
-				return(<NewCustomer />)
+				return(<PickProjects />)
 				break
 			}
-			case "CustomerViewCustomer":{
-				return(<ViewCustomer />)
-				break
-			}
-			case "CustomerEditCustomer":{
-				return(<EditCustomer />)
-				break
-			}
-			case "CustomerMakePayment":{
-				return(<MakePayment />)
-				break
-			}
+            case "ViewProject":{
+                return(<ViewProject />)
+                break
+            }
+
 			default : {
-				return <h2> Welcome to Customer App </h2>
+				return <h2> Welcome to Project App </h2>
 			}
 		}
 	}
 	getSideMenue(){
-		var url = [{name:"Home" ,viewID:"Home"},{name:this.props.app.name ,viewID:"Customer"}]
+		var url = [{name:"Home" ,viewID:"Home"},{name:this.props.app.name ,viewID:"Project"}]
 		if(this.props.url[2] == null){
 			return(<SideMenu menuItems={this.props.app.menuItems} name={this.props.app.name} concatURL={url} />)
 		}
+		/*
 		var tabViewID = this.props.url[2].viewID
 		switch(tabViewID){
-			case "CustomerAll":{
-				return(<SideMenu menuItems={this.props.app.menuItems} name={this.props.app.name} concatURL={url} />)
-				break	
-			}
-			case "CustomerNewCustomer":{
+			case "ProjectAll":{
 				return(<SideMenu menuItems={this.props.app.menuItems} name={this.props.app.name} concatURL={url} />)
 				break
 			}
-			case "CustomerViewCustomer":{
-				return(<SideMenu menuItems={this.props.app.tabs.ViewCustomer.menuItems} name={this.props.app.tabs.ViewCustomer.name} concatURL={url} />)
-				break
-			}
-			case "CustomerEditCustomer":{
-				return(<SideMenu menuItems={this.props.app.tabs.ViewCustomer.menuItems} name={this.props.app.tabs.ViewCustomer.name} concatURL={url} />)
-				break
-			}
-			case "CustomerMakePayment":{
-				return(<SideMenu menuItems={this.props.app.tabs.ViewCustomer.menuItems} name={this.props.app.tabs.ViewCustomer.name} concatURL={url} />)
-				break
-			}
-			default : {
-				return <h2> Welcome to Customer App </h2>
-			}
-		}
+            case "ViewProject":{
+                return(<SideMenu menuItems={this.props.app.tabs.ViewProject.menuItems} name={this.props.app.tabs.ViewProject.name} concatURL={url} />)
+                break
+            }
+
+			default : {*/
+				return <h2> Projects </h2>
+			//}
+		//}
 	}
 	render() {
 		return  (
 			<div class="row">
 			  <div class="col-md-3">
-			  {this.getSideMenue()}
+                  {this.getSideMenue()}
 			  </div>
 			  <div class="col-md-9">
-			  {this.getTab()}
+                  {this.getTab()}
+
 			  </div>
 			</div>
 			)
